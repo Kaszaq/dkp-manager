c4820d52a2922f9916481d5a749c5a61502b5fe4
kaszaq <kaszaq@kaszaq-pc>
2013-12-19 13:10:01 +0100

    Wycofano pakiet zmian: 080638ed6ccb

--- mng_standbyGUI.lua
+++ mng_standbyGUI.lua
@@ -22,7 +22,6 @@ function A:CreateStandbyGUI()
 	v.resetdkpButton:Show()
 	
 	v.testFunctionButton=gs:CreateButton(name.."_testFunctionButton", "TEST", 45, "LEFT", v.printaltButton,"RIGHT", 1, 25)
-	v.testFunctionButton:Hide()
 	
 	
 	v.inputBox:SetScript("OnEnterPressed", function(self)
